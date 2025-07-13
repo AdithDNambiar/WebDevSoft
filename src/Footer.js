71326fa2136833fd19514887073abf82c1efaf79
Adith Nambiar <adiadithdnambiar@gmail.com>
2025-07-13 15:09:55 +0530

    Fix logo path case in footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className="footer-grid">
 
           <div className="footer-col">
-            <div className="logo">
-              <img src="/LOGO.png" alt="WebDevSoft Logo" className="logo-img" loading="lazy" />
+            <div className="footer-logo">
+              <img src="/Logo.png" alt="WebDevSoft Logo" className="logo-img" loading="lazy" />
               <p className="logo-heading">WebDevSoft</p>
             </div>
             <br />
@@ -57,30 +57,30 @@ const Footer = () => {
           <div className="footer-col">
             <h4>Follow Us</h4>
             <div className="social-links">
-             <a href="https://www.facebook.com/WebDevsoft/" target="_blank" rel="noopener noreferrer">
-                <img src="facebook.png" alt="Facebook" className="social-icon" loading="lazy" />
+              <a href="https://www.facebook.com/WebDevsoft/" target="_blank" rel="noopener noreferrer">
+                <img src="/facebook.png" alt="Facebook" className="social-icon" loading="lazy" />
               </a>
               <a href="https://twitter.com/WebDevSoft" target="_blank" rel="noopener noreferrer">
-                <img src="twitter.png" alt="Twitter" className="social-icon" loading="lazy" />
+                <img src="/twitter.png" alt="Twitter" className="social-icon" loading="lazy" />
               </a>
               <a href="https://www.instagram.com/webdevsoftit" target="_blank" rel="noopener noreferrer">
-                <img src="instagram.png" alt="Instagram" className="social-icon" loading="lazy" />
+                <img src="/instagram.png" alt="Instagram" className="social-icon" loading="lazy" />
               </a>
               <a href="https://www.linkedin.com/company/webdevsoft-it/" target="_blank" rel="noopener noreferrer">
-                <img src="linkedin.jpg" alt="LinkedIn" className="social-icon" loading="lazy" />
+                <img src="/linkedin.jpg" alt="LinkedIn" className="social-icon" loading="lazy" />
               </a>
             </div>
             <div className="footer-partners">
               <h4 className="partner-heading">Supported By</h4>
               <div className="partner-logos">
                 <div className="logo-box">
-                  <img src="msme-seeklogo.svg" alt="MSME Logo" className="footer-logo" loading="lazy" />
+                  <img src="/msme-seeklogo.svg" alt="MSME Logo" className="footer-logo" loading="lazy" />
                 </div>
                 <div className="logo-box">
-                  <img src="startup-india-hub-logo-vector.svg" alt="Startup India" className="footer-logo" loading="lazy" />
+                  <img src="/startup-india-hub-logo-vector.svg" alt="Startup India" className="footer-logo" loading="lazy" />
                 </div>
                 <div className="logo-box">
-                  <img src="skill-india-seeklogo.png" alt="SkillUp Trainer" className="footer-logo" loading="lazy" />
+                  <img src="/skill-india-seeklogo.png" alt="SkillUp Trainer" className="footer-logo" loading="lazy" />
                 </div>
               </div>
             </div>
